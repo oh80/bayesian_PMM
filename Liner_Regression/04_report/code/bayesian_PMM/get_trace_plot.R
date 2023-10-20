26b2be92940fae6c0b392c4b4c7b23d3785110aa
oh80 <ogawa-hajime-vs@ynu.jp>
2023-10-20 11:05:01 +0900

    Rct MAR\NMAR build first commit

--- Liner_Regression/04_report/code/bayesian_PMM/get_trace_plot.R
+++ Liner_Regression/04_report/code/bayesian_PMM/get_trace_plot.R
@@ -2,35 +2,25 @@ main <- function(){
   library(ggplot2)
   # read sample
   missing_rate <- 0.5
-  missing_type <- "MCAR"
+  missing_type <- "MAR"
   file_name <- paste0(missing_type , "_", missing_rate, ".obj")
   path <- here::here("Liner_regression", "03_analyze","output", "bayesian_PMM", file_name)
   sample <- readRDS(path)
   
   # draw a graph
-  beta <- 10
-  missing_value <- 1
-  plot <- draw_trace_plot(sample, beta, missing_value, missing_type)
-  
-  #save plot
-  file_name <- paste0(missing_type, "trace_plot.pdf")
-  path <- here::here("Liner_regression", "04_report", "output","bayesian_PMM",file_name)
-  ggsave(filename = path, plot = plot, device = "pdf",  width = 8, height = 4)
+  plot <- draw_trace_plot(sample)
+  return(plot)
 }
 
 
 
-draw_trace_plot <- function(sample, beta, missing_value, missing_type){
-  sample1 <- sample[[missing_value]]
+draw_trace_plot <- function(sample){
+  sample1 <- sample[[1]]
   sample1_beta <- sample1$beta_sample|> t()
-  data <- data.frame("sample" = sample1_beta[,beta], "times" = 1:1000)
+  data <- data.frame("sample" = sample1_beta[,1], "times" = 1:1000)
   output <- ggplot(data = data, mapping = aes(x = times, y = sample))+
-    geom_line(colour = "seagreen")+
-    labs(title = missing_type)
-    
-    
+    geom_line()
   return(output)
 }
 
-
-main()
+plot <- main()
---
+++ RCT/02_build/code/MAR/build.R
@@ -0,0 +1,78 @@
+main <- function(){
+  path <- here::here("RCT","01_data","data","data.obj")
+  data <- readRDS(path)
+  
+  set.seed(428)
+  missing_rate <- 0.3
+  missing_data <- get_missing_indicater(data, missing_rate) |> 
+    get_missing_data()
+  
+  save(missing_data, missing_rate)
+}
+
+
+get_prob <- function(missing_rate){
+  prob0 <- missing_rate
+  prob1 <- (1 - prob0) * missing_rate
+  prob2 <- (1 - prob1 - prob0) * missing_rate
+  prob3 <- (1 - prob2 - prob1 - prob0)
+  
+  output <- c(prob0, prob1, prob2, prob3)
+  return(output)
+}
+
+
+get_missing_indicater <- function(data, missing_rate){
+  N <- length(data$X1)
+  effect_for_missing <- c(3, 0.5, 0.5, 0.5, 0.5, 0.5)
+  epsilon <- rnorm(N , mean = 0, sd = 2)
+  
+  missing_predict <- rep(0, N)
+  for (i in 1:N) {
+    values <- c(data$Treatment[i], data$X1[i], data$X2[i], data$X3[i], data$X4[i], data$X5[i])
+    missing_predict[i] <- sum(values * effect_for_missing) + epsilon[i]
+  }
+  
+  data <- data |> dplyr::mutate("missing_predict" = missing_predict) |> 
+    dplyr::arrange(-missing_predict)
+  
+  pattern_prob <- get_prob(missing_rate)
+  missing_num <- N*pattern_prob
+  
+  data <- data |> dplyr::mutate("R" = rep(3,N))
+  pattern_devid <- c(1,missing_num[1], missing_num[1]+1, missing_num[1]+missing_num[2],
+                     missing_num[1]+missing_num[2]+1, missing_num[1]+missing_num[2]+missing_num[3])
+  data$R[pattern_devid[1]: pattern_devid[2]] <- 0
+  data$R[pattern_devid[3]: pattern_devid[4]] <- 1
+  data$R[pattern_devid[5]: pattern_devid[6]] <- 2
+  
+  output <- data |> dplyr::select(-missing_predict)
+  return(output)
+}
+
+
+get_missing_data <- function(data){
+  N <- length(data$X1)
+  for (i in 1:N) {
+    if(data$R[i] == 0){
+      data$Y1[i] = data$Y2[i] = data$Y3[i] = NaN
+    }
+    if(data$R[i] == 1){
+      data$Y2[i] = data$Y3[i] = NaN
+    }
+    if(data$R[i] == 2){
+      data$Y3[i] = NaN
+    }
+  }
+  output <- data
+  return(output)
+}
+
+save <- function(missing_data, missing_rate){
+  file_name <- paste0("MAR_", missing_rate, ".obj")
+  path <- here::here("RCT","02_build","data","MAR",file_name)
+  saveRDS(missing_data, path)
+}
+
+
+main()
