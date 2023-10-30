6933b8336a0995c74de0088ddcd04c141a742c36
oh80 <ogawa-hajime-vs@ynu.jp>
2023-10-30 18:20:49 +0900

    ACMV first commit

---
+++ RCT/03_analyze/code/ACMV/analyze.R
@@ -0,0 +1,118 @@
+main <- function(){
+  #read data
+  missing_type <- "MCAR"
+  missing_rate <- 0.1
+  
+  file_name <- paste0(missing_type,"_", missing_rate, ".", "obj")
+  path <- here::here("RCT","02_build","data", missing_type ,file_name)
+  data <- readRDS(path)
+  
+  #read functions
+  code_path <- here::here("RCT","03_analyze","code","ACMV","imputation.R")
+  source(code_path)
+  
+  #param set
+  D <- 10
+  delta <- 0
+  
+  #imputation
+  psude_complete_data <- multiple_imputation(data, D, delta)
+  
+  #estimation
+  estimated_values <- esimate_each_data(psude_complete_data)
+  
+  #combine results
+  estimate_result <- combine(estimated_values)
+  
+  #save estimated values
+  file_name <- paste0(missing_type ,missing_rate,"delta",delta,".obj")
+  save(estimate_result, file_name)
+}
+
+
+multiple_imputation <- function(data, D, delta){
+  output <- list()
+  for (d in 1:D) {
+    set.seed(d)
+    output[[d]] <- single_imputation(data, delta)
+  }
+  return(output)
+}
+
+
+esimate_each_data <- function(psude_complete_data){
+  D <- length(psude_complete_data)
+  output <- list()
+  for (d in 1:D) {
+    data <- psude_complete_data[[d]]
+    estimated_values <- list()
+    estimated_values[[1]] <- estimatr::lm_robust(formula = Y1 ~ X1+X2+X3+X4+X5+Treatment,
+                                                 data = data,se_type = "stata")
+    estimated_values[[2]] <- estimatr::lm_robust(formula = Y2 ~ X1+X2+X3+X4+X5+Treatment,
+                                                 data = data,se_type = "stata")
+    estimated_values[[3]] <- estimatr::lm_robust(formula = Y3 ~ X1+X2+X3+X4+X5+Treatment,
+                                                 data = data,se_type = "stata")
+    output[[d]] <- estimated_values
+    
+  }
+  return(output)
+}
+
+
+combine <- function(estimated_values){
+  D = length(estimated_values)
+  
+  combined_values1 <- rep(0, 7)
+  for (d in 1:D) {
+    combined_values1 <- combined_values1 + estimated_values[[d]][[1]]$coefficients/D
+  }
+  
+  combined_values2 <- rep(0, 7)
+  for (d in 1:D) {
+    combined_values2 <- combined_values2 + estimated_values[[d]][[2]]$coefficients/D
+  }
+  
+  combined_values3 <- rep(0, 7)
+  for (d in 1:D) {
+    combined_values3 <- combined_values3 + estimated_values[[d]][[3]]$coefficients/D
+  }
+  
+  var1 <- rep(0, 7)
+  for (d in 1:D) {
+    var1 <- var1 + 1/D * (estimated_values[[d]][[1]]$std.error)^2 + 
+      (1+1/D)*(1/(D-1))*(combined_values1 - estimated_values[[d]][[1]]$coefficients)^2
+  }
+  
+  var2 <- rep(0, 7)
+  for (d in 1:D) {
+    var2 <- var2 + 1/D * (estimated_values[[d]][[2]]$std.error)^2 + 
+      (1+1/D)*(1/(D-1))*(combined_values2 - estimated_values[[d]][[2]]$coefficients)^2
+  }
+  
+  var3 <- rep(0, 7)
+  for (d in 1:D) {
+    var3 <- var3 + 1/D * (estimated_values[[d]][[3]]$std.error)^2 + 
+      (1+1/D)*(1/(D-1))*(combined_values3 - estimated_values[[d]][[3]]$coefficients)^2
+  }
+  
+  output <- list()
+  output$estimated_values1 <- combined_values1
+  output$estimated_values2 <- combined_values2
+  output$estimated_values3 <- combined_values3
+  output$var1 <- var1
+  output$var2 <- var2
+  output$var3 <- var3
+  
+  return(output)
+}
+
+
+save <- function(results, file_name){
+  path <- here::here("RCT", "03_analyze","output", "ACMV", file_name)
+  saveRDS(results, path)
+}
+
+
+main()
+
+
--- RCT/03_analyze/code/ACMV/imputation.R
+++ RCT/03_analyze/code/ACMV/imputation.R
@@ -155,4 +155,37 @@ impute_data <- function(data, imputed_pattern, delta){
   }
 
 
-i <- impute_data(data, 0, 0)
+single_imputation <- function(data, delta){
+  pattern <- data$R
+  n <- length(data$X1)
+  
+  complete_data <- data |> dplyr::filter(R == 3)
+  R2_data <- data |> dplyr::filter(R == 2)
+  
+  #one time imputation
+  R3_imputed_data <- impute_data(data, imputed_pattern = 2, delta = delta)
+  R2_impute_data <- impute_data(data, imputed_pattern = 1, delta = delta) |> 
+    dplyr::mutate(R = R + 1)
+  R1_impute_data <- impute_data(data, imputed_pattern = 0, delta = delta) |> 
+    dplyr::mutate(R = R + 1)
+  
+  #two times imputation
+  R3R2_imputed_data <- impute_data(data = dplyr::bind_rows(complete_data, R2_impute_data),
+                                   imputed_pattern = 2, delta = 0) |> 
+    dplyr::mutate(R = R - 1)
+  
+  R1R2_imputed_data <- impute_data(data = dplyr::bind_rows(complete_data, R2_data, R1_impute_data),
+                                   imputed_pattern = 1, delta = 0) |> 
+    dplyr::mutate(R = R + 1)
+  
+  #three times imputation
+  R1R2R3_imputed_data <- impute_data(data = dplyr::bind_rows(complete_data, R1R2_imputed_data),
+                                     imputed_pattern = 2, delta = 0) |> 
+    dplyr::mutate(R = R - 2)
+  
+  output <- dplyr::bind_rows(complete_data, R3_imputed_data, R3R2_imputed_data, R1R2R3_imputed_data)
+  
+  
+  return(output)
+}
+
