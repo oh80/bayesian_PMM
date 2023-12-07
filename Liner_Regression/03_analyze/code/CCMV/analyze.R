0049524f28554a4acd90696c89df7c18d9b8c3ee
oh80 <ogawa-hajime-vs@ynu.jp>
2023-12-07 15:33:03 +0900

    modify CCMV table

--- Liner_Regression/03_analyze/code/CCMV/analyze.R
+++ Liner_Regression/03_analyze/code/CCMV/analyze.R
@@ -1,7 +1,7 @@
 main <- function(){
   #read_data
-  missing_rate <- 0.1
-  missing_type <- "MAR"
+  missing_rate <- 0.5
+  missing_type <- "MCAR"
   file_name <- paste0(missing_type , "_", missing_rate, ".obj")
   path <- here::here("Liner_regression", "02_build","data", file_name)
   data <- readRDS(path)
